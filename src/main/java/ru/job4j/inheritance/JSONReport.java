16d843c017369c9bd3030770796c9055d1e7494a
Dmitriy Radomirov <dibradim@gmail.com>
2022-03-31 11:42:54 +0300

    5. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,8 +2,11 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
 
-    @Override
-    public String generate(String name, String body) {
-        return "{" + System.lineSeparator() + "\t" + "\"name\"" + " : " + "\"" + name + "\"" + "," + System.lineSeparator() + "\t" + "\"body\"" + " : " + "\"" + body + "\"" + System.lineSeparator() + "}";
+        public String generate(String name, String body) {
+        return "{" + System.lineSeparator()
+                + "\t" + "\"name\"" + " : " + "\"" + name + "\"" + ","
+                + System.lineSeparator()
+                + "\t" + "\"body\"" + " : " + "\"" + body + "\""
+                + System.lineSeparator() + "}";
     }
 }
