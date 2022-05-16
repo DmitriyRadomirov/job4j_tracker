18c27c4830f3b6e5c26665cbb385eadcb80ed96f
Dmitriy Radomirov <dibradim@gmail.com>
2022-05-16 12:35:00 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #297319]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -66,12 +66,13 @@ public class Tracker {
     }
 
     public boolean delete(int id) {
-        int indexOf = indexOf(id);
-        int start = indexOf + 1;
-        int length = size - 1;
-        System.arraycopy(items, start, items, indexOf, length);
-        items[size - 1] = null;
-        size--;
-        return indexOf(id) != -1;
+        int index = indexOf(id);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+            return true;
+        }
+        return false;
     }
 }
