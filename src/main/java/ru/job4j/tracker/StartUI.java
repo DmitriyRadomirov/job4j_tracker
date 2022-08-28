f357ac7759bb3567041434c8827c8033e4bbf6b2
Dmitriy Radomirov <dibradim@gmail.com>
2022-08-28 16:39:12 +0300

    2.2. Реализация класса StartUI. Вывод меню [#500743]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,11 +1,41 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class StartUI {
-   public static void main(String[] args) {
-        Item item = new Item();
-        System.out.println(item);
+    //1. Метод init() - инициализирует приложение и запускает выполнение различных пользовательских действий;
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Select: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
+                run = false;
+            }
         }
-}
+    }
 
+    //2. Метод showMenu() - выводит на экран меню доступных пользовательских действий;
+
+    private void showMenu() {
+        String[] menu = {
+                "Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"
+        };
+
+        //Чтобы вывести в консоль этот массив поэлементно с обозначением номера пункта меню - используем следующий цикл:
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
+    }
+}
