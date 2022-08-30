d7ce634c98bced820f6b219ecead6c4550cf12b2
Dmitriy Radomirov <dibradim@gmail.com>
2022-08-30 11:54:23 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public int run() {
+        return 0;
+    }
+
+    @Override
+    public int passengers(int count) {
+        return 0;
+    }
+
+    @Override
+    public int refueling(int numOfLiters, int price) {
+        return 0;
+    }
+}
--- src/main/java/ru/job4j/poly/Transport.java
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -1,5 +1,10 @@
 package ru.job4j.poly;
 
 public interface Transport {
+    int run();
+
+    int passengers(int count);
+
+    int refueling(int numOfLiters, int price);
 
 }
