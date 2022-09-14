77bab9e6f9ed3119bcf765ce2c13c62bd50b7739
Dmitriy Radomirov <dibradim@gmail.com>
2022-09-14 20:03:42 +0300

    123

--- src/main/java/ru/job4j/tracker/CreateAction.java
+++ src/main/java/ru/job4j/tracker/CreateAction.java
@@ -1,19 +1,23 @@
 package ru.job4j.tracker;
 
 public class CreateAction implements UserAction {
+    private final Output out;
+
+    public CreateAction(Output out) {
+        this.out = out;
+    }
+
     @Override
     public String name() {
-        return "Add new Item";
+        return "Create";
     }
 
     @Override
     public Boolean execute(Input input, Tracker tracker) {
-        System.out.println("=== Create a new Item ===");
-        String name = input.askStr("Enter name");
+        out.println("=== Create a new Item ====");
+        String name = input.askStr("Enter name: ");
         Item item = new Item(name);
         tracker.add(item);
-        System.out.println("Добавленная заявка: " + item);
-
-        return  true;
+        return true;
     }
 }
