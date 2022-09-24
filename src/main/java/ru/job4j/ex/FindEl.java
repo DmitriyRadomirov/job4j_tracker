ccb638256ac5787196a766c368eb3ec30d08297e
Dmitriy Radomirov <dibradim@gmail.com>
2022-09-24 20:28:24 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,34 @@
+package ru.job4j.ex;
+
+public class FindEl  {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (key.equals(value[index])) {
+                rsl = index;
+                break;
+            }
+        }
+            if (rsl == -1) {
+                    throw new ElementNotFoundException("Element not found");
+            }
+        return rsl;
+    }
+
+    public static class ElementNotFoundException extends Exception {
+        public ElementNotFoundException(String message) {
+            super(message);
+        }
+
+        public static void main(String[] args) {
+            try {
+                String[] strings = {"AAA", "BBB", "SSS"};
+                indexOf(strings, "SSS");
+            } catch (ElementNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
