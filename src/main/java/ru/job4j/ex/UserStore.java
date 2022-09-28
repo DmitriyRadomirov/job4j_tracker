bad6283541275f99692669d9186c3ad870152b00
Dmitriy Radomirov <dibradim@gmail.com>
2022-09-28 13:07:19 +0300

    0.5. Иерархия исключений и множественный catch. [#219368]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -1,41 +1,41 @@
 package ru.job4j.ex;
 
-import ru.job4j.tracker.Item;
-
-import java.util.Arrays;
-
 public class UserStore {
 
     public static User findUser(User[] users, String login) throws UserNotFoundException {
-        for (int i = 0; i < users.length; i++) {
-            if (String.valueOf(users[i]).equals(login)) {
-                User userFind = users[i];
+        User user = null;
+        for (User fdUser: users) {
+            if (fdUser.getUsername().equals(login)) {
+                user = fdUser;
                 break;
             } else {
                 throw new UserNotFoundException("User not found");
+            }
         }
-        return null;
-    }
-
-    public static boolean validate(User user) throws UserInvalidException {
-            if (user.isValid()){
-                break;
+            return user;
         }
 
-        int len = login.length();
-                if (len <= 3) {
-            throw new UserInvalidException("User invalid");
+        public static boolean validate(User user) throws UserInvalidException {
+            if (!user.isValid() || user.getUsername().length() < 3) {
+                throw new UserInvalidException("User invalid");
+            }
+            return true;
         }
-        return false;
+
+        public static void main(String[] args) {
+            User[] users = {
+                    new User("Petr Arsentev", true)
+            };
+            try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+            } catch (UserInvalidException usIE) {
+                usIE.printStackTrace();
+            } catch (UserNotFoundException usNFE) {
+                usNFE.printStackTrace();
     }
 
-    public static void main(String[] args) {
-        User[] users = {
-                new User("Petr Arsentev", true)
-        };
-        User user = findUser(users, "Petr Arsentev");
-        if (validate(user)) {
-            System.out.println("This user has an access");
-        }
-    }
 }
+    }
