4b181abc7aaf944b915a7797728c43137d4f6477
Dmitriy Radomirov <dibradim@gmail.com>
2022-09-29 11:26:07 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #347024] Исправленый 2

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -4,38 +4,38 @@ public class UserStore {
 
     public static User findUser(User[] users, String login) throws UserNotFoundException {
         User user = null;
-        for (User fdUser: users) {
+        for (User fdUser : users) {
             if (fdUser.getUsername().equals(login)) {
                 user = fdUser;
                 break;
-            } else {
-                throw new UserNotFoundException("User not found");
             }
         }
-            return user;
+        if (user == null) {
+            throw new UserNotFoundException("User not found");
         }
+        return user;
+    }
 
-        public static boolean validate(User user) throws UserInvalidException {
-            if (!user.isValid() || user.getUsername().length() < 3) {
-                throw new UserInvalidException("User invalid");
-            }
-            return true;
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("User invalid");
         }
+        return true;
+    }
 
-        public static void main(String[] args) {
-            User[] users = {
-                    new User("Petr Arsentev", true)
-            };
-            try {
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        try {
             User user = findUser(users, "Petr Arsentev");
             if (validate(user)) {
                 System.out.println("This user has an access");
             }
-            } catch (UserInvalidException usIE) {
-                usIE.printStackTrace();
-            } catch (UserNotFoundException usNFE) {
-                usNFE.printStackTrace();
+        } catch (UserInvalidException usIE) {
+            usIE.printStackTrace();
+        } catch (UserNotFoundException usNFE) {
+            usNFE.printStackTrace();
+        }
     }
-
 }
-    }
