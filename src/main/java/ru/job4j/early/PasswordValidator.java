e2c5d47a163a73a758e63bb1f7ad8d244404ade5
Dmitriy Radomirov <dibradim@gmail.com>
2022-10-03 11:12:51 +0300

    4. Принципы раннего возврата и охранных выражений [#504875] v1

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -1,32 +1,42 @@
-/* package ru.job4j.early;
+package ru.job4j.early;
+
+import java.util.Locale;
 
 public class PasswordValidator {
     public static String validate(String password) {
         if (password == null) {
             throw new IllegalArgumentException("Password is null");
         }
-        if (password.length() <= 8 || password.length() >= 32) {
+        if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Password must be at least 8 and no more than 32 characters long");
         }
-        if (!Character.isUpperCase(Integer.parseInt(password))) {
+        if (password.equals(password.toLowerCase())) {
             throw new IllegalArgumentException("Password must contain at least one uppercase letter");
         }
-        if (!Character.isLowerCase(Integer.parseInt(password))) {
+        if (password.equals(password.toUpperCase())) {
             throw new IllegalArgumentException("Password must contain at least one lowercase letter");
         }
-        if (!Character.isDigit(Integer.parseInt(password))) {
+        if (!password.matches("(.*[0-9]+.*)")) {
             throw new IllegalArgumentException("Password must be including a number");
         }
-        if (!password.matches("[@#$%^&+=]")) {
+        if (password.toLowerCase().contains("qwerty")) {
+            throw new IllegalArgumentException("Рassword must not contain \"qwerty\" ");
+        }
+        if (password.toLowerCase().contains("12345")) {
+            throw new IllegalArgumentException("Рassword must not contain \"12345\" ");
+        }
+        if (password.toLowerCase().contains("password")) {
+            throw new IllegalArgumentException("Рassword must not contain \"password\" ");
+        }
+        if (password.toLowerCase().contains("admin")) {
+            throw new IllegalArgumentException("Рassword must not contain \"admin\" ");
+        }
+        if (password.toLowerCase().contains("user")) {
+            throw new IllegalArgumentException("Рassword must not contain \"user\" ");
+        }
+        if (!password.matches("(.*[@#$%^&+=].*)")) {
             throw new IllegalArgumentException("Password must contain special characters");
         }
-        if ()
+        return password;
     }
-
-
-
-* Пароль не содержит подстрок без учета регистра: qwerty, 12345, password, admin, user.
-* Без учета регистра,
-* значит что, например, ни qwerty ни QWERTY ни qwErty и т.п. не должно быть в составе пароля.
-
- */
+}
---
+++ src/test/java/ru/job4j/early/PasswordValidatorTests.java
@@ -0,0 +1,121 @@
+package ru.job4j.early;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PasswordValidatorTests {
+
+    @Test
+    public void whenPasswordIncorrectLengthLess() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("aaaa");
+                });
+        assertThat(exception.getMessage()).isEqualTo("Password must be at least 8 and no more than 32 characters long");
+    }
+
+    @Test
+    public void whenPasswordIncorrectLengthLonger() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("aaaabbbbccccddddeeeeffffgggghhhhk");
+                });
+        assertThat(exception.getMessage()).isEqualTo("Password must be at least 8 and no more than 32 characters long");
+    }
+
+    @Test
+    public void whenPasswordIncorrectNoUppercaseLetter() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("aaaabbbb");
+                });
+        assertThat(exception.getMessage()).isEqualTo("Password must contain at least one uppercase letter");
+    }
+
+    @Test
+    public void whenPasswordIncorrectNoLowercaseLetter() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("AAAABBBB");
+                });
+        assertThat(exception.getMessage()).isEqualTo("Password must contain at least one lowercase letter");
+    }
+
+    @Test
+    public void whenPasswordIncorrectNoDigit() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("AAAA+aAa");
+                });
+        assertThat(exception.getMessage()).isEqualTo("Password must be including a number");
+    }
+
+    @Test
+    public void whenPasswordIncorrectQwerty() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("Qwerty+1Aa");
+                });
+        assertThat(exception.getMessage()).isEqualTo("Рassword must not contain \"qwerty\" ");
+    }
+
+    @Test
+    public void whenPasswordIncorrect12345() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("12345+1Aa");
+                });
+        assertThat(exception.getMessage()).isEqualTo("Рassword must not contain \"12345\" ");
+    }
+
+    @Test
+    public void whenPasswordIncorrectPassword() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("password+1Aa");
+                });
+        assertThat(exception.getMessage()).isEqualTo("Рassword must not contain \"password\" ");
+    }
+
+    @Test
+    public void whenPasswordIncorrectAdmin() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("adMin+1Aa");
+                });
+        assertThat(exception.getMessage()).isEqualTo("Рassword must not contain \"admin\" ");
+    }
+
+    @Test
+    public void whenPasswordIncorrectUser() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("useR+1Aa");
+                });
+        assertThat(exception.getMessage()).isEqualTo("Рassword must not contain \"user\" ");
+    }
+
+    @Test
+    public void whenPasswordIncorrectNoSpecialChar() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    PasswordValidator.validate("aaaabbbb1Aa");
+                });
+        assertThat(exception.getMessage()).isEqualTo("Password must contain special characters");
+    }
+}
+
