90739e4e5df480c2a0b16f84fc335fd34f08d15d
Dmitriy Radomirov <dibradim@gmail.com>
2022-11-11 21:55:24 +0300

    3. final [#334073 #358162] correction v 1

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -23,7 +23,7 @@ public final class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        int engine = getName().equals("A380") ? 4 : COUNT_ENGINE;
+        int engine = "A380".equals(getName()) ? 4 : COUNT_ENGINE;
         System.out.println("Количество двигателей равно:" + engine);
     }
 
